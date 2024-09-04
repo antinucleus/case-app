2e4fb68c5ede2f879a53c1673c17b2559e7fb1fe
antinucleus <antinucleuss@gmail.com>
2024-09-04 09:45:31 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['expo', 'prettier', 'universe/native'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    quotes: [2, 'single'],
+  },
+};
